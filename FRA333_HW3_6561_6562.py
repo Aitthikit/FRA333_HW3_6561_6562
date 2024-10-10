7340c179dff7d7cf53f00afa2e790fd3725ae88e
Aitthikit <123627998+Aitthikit@users.noreply.github.com>
2024-10-10 19:07:59 +0700

    Jacobe

--- FRA333_HW3_6561_6562.py
+++ FRA333_HW3_6561_6562.py
@@ -2,9 +2,8 @@
 # ในกรณีที่มีการสร้าง function อื่น ๆ ให้ระบุว่า input-output คืออะไรด้วย
 '''
 ชื่อ_รหัส(ธนวัฒน์_6461)
-1.อิทธิกฤต_6562
-2.
-3.
+1.อสมา_6561
+2.อิทธิกฤต_6562
 '''
 import HW3_utils
 import roboticstoolbox as rtb
@@ -20,15 +19,24 @@ def endEffectorJacobianHW3(q:list[float])->list[float]:
     p_03 = P[:,2]
     p_0e = p_e
     R_e0 = np.linalg.inv(R_e)
+    R_e1 = R_e0 @ R[:,:,0]
+    R_e2 = R_e0 @ R[:,:,1]
+    R_e3 = R_e0 @ R[:,:,2]
+    # print (R_e1,R_e2)
     z_1 = np.array([0.0,0.0,1.0]).reshape(3,1)
-    z_2 = np.array([math.sin(q[1]),math.cos(q[1]),0.0]).reshape(3,1)
-    z_3 = np.array([math.sin(q[1]),math.cos(q[1]),0.0]).reshape(3,1)
-    z_e1 = R_e0 @ z_1
-    z_e2 = R_e0 @ z_2
-    z_e3 = R_e0 @ z_3
-    J_01 = np.vstack((R_e0 @ base.cross(z_1,base.vector_diff(p_0e,p_01,'r')).reshape(3,1),z_e1))
-    J_02 = np.vstack((R_e0 @ base.cross(z_2,base.vector_diff(p_0e,p_02,'r')).reshape(3,1),z_e2))
-    J_03 = np.vstack((R_e0 @ base.cross(z_3,base.vector_diff(p_0e,p_03,'r')).reshape(3,1),z_e3))
+    # z_2 = np.array([math.sin(q[1]),-math.cos(q[1]),0.0]).reshape(3,1)
+    # z_3 = np.array([math.sin(q[1]),-math.cos(q[1]),0.0]).reshape(3,1)
+    z_2 = np.array([0.0,0.0,1.0]).reshape(3,1)
+    z_3 = np.array([0.0,0.0,1.0]).reshape(3,1)
+    z_01 = R[:,:,0] @ z_1
+    z_02 = R[:,:,1] @ z_2
+    z_03 = R[:,:,2] @ z_3
+    z_e1 = R_e1 @ z_1
+    z_e2 = R_e2 @ z_2
+    z_e3 = R_e3 @ z_3
+    J_01 = np.vstack((R_e0 @ base.cross(z_01,base.vector_diff(p_0e,p_01,'r')).reshape(3,1),z_e1))
+    J_02 = np.vstack((R_e0 @ base.cross(z_02,base.vector_diff(p_0e,p_02,'r')).reshape(3,1),z_e2))
+    J_03 = np.vstack((R_e0 @ base.cross(z_03,base.vector_diff(p_0e,p_03,'r')).reshape(3,1),z_e3))
     J_e = np.hstack((J_01,J_02,J_03))
     return J_e
 #==============================================================================================================#
@@ -38,8 +46,8 @@ def checkSingularityHW3(q:list[float])->bool:
     J_e = endEffectorJacobianHW3(q)
     J_re = J_e[:3,:]
     value = base.det(J_re)
-    print(value)
-    if value < 0.001:
+    # print(abs(value))
+    if abs(value) < 0.001:
         return 1
     else:
         return 0
@@ -50,9 +58,12 @@ def computeEffortHW3(q:list[float], w:list[float])->list[float]:
     J_e = endEffectorJacobianHW3(q)
     J_ret = np.transpose(J_e)
     w_t = np.array(w)
+    print(w_t)
     tau = J_ret @ w_t
     return tau
 #==============================================================================================================#
-# print(endEffectorJacobianHW3([0.0,0.0,0.0]))
-# print(checkSingularityHW3([-0.6,-math.pi/2,0.0]))
-print(computeEffortHW3([0.0,0.0,0.0],[1.0,0.0,0.0,1.0,0.0,0.0]))
+q = [0.0,-math.pi/2,-0.2] #joint [joint1,joint2,joint3] recommend[-0.6,-math.pi/2,0.0] for singularity test
+w = [1.0,1.0,5.0,1.0,2.0,1.0]# wrench [Fx,Fy,Fz,Mx,My,Mz]
+print(endEffectorJacobianHW3(q))
+print(checkSingularityHW3(q))
+print(computeEffortHW3(q,w))
--- testScript.py
+++ testScript.py
@@ -1,62 +1,84 @@
 # file สำหรับตรวจคำตอบ
 # ในกรณีที่มีการสร้าง function อื่น ๆ ให้ระบุว่า input-output คืออะไรด้วย
 '''
-ชื่อ_รหัส(ex: ธนวัฒน์_6461)
-1.อิทธิกฤต_6562
-2.
-3.
+ชื่อ_รหัส(ธนวัฒน์_6461)
+1.อสมา_6561
+2.อิทธิกฤต_6562
 '''
 import HW3_utils
 import roboticstoolbox as rtb
 import numpy as np
 from spatialmath import SE3
 from math import pi,radians
-#    d_1 = 0.0892
-#     a_2 = -0.425
-#     a_3 = -0.39243
-#     d_4 = 0.109
-#     d_5 = 0.093
-#     d_6 = 0.082
-#===========================================<ตรวจคำตอบข้อ 1>====================================================#
-#code here
-def proofOne(q:list[float])->list[float]:
-    robot = rtb.DHRobot(
+import FRA333_HW3_6561_6562 as hand
+d_1 = 0.0892
+a_2 = -0.425
+a_3 = -0.39243
+d_4 = 0.109
+d_5 = 0.093
+d_6 = 0.082
+robot = rtb.DHRobot(
     [
-        rtb.RevoluteMDH(d=0.0892 , offset = pi),
+        rtb.RevoluteMDH(d= d_1 , offset = pi),
         rtb.RevoluteMDH(alpha = pi/2 ),
-        rtb.RevoluteMDH(a=-0.425),
+        rtb.RevoluteMDH(a=a_2),
     ],
     name = "RRR_Robot"
     )
-    tool_frame = SE3(-0.47443, -0.093,0.109) @ SE3.RPY(0.0,-pi/2,0.0)  # Adjust the position and orientation as needed
-
-    # Assign the tool frame to the robot
-    robot.tool = tool_frame
-    # print(robot)
-    # print(robot.fkine([0.0,0.0,0.0]))
-    J_e = [robot.jacob0(q)[0],robot.jacob0(q)[1],robot.jacob0(q)[2]]
-    return robot.jacobe(q)
+tool_frame = SE3((a_3-d_6),-d_5,d_4) @ SE3.RPY(0.0,-pi/2,0.0)
+robot.tool = tool_frame
+#===========================================<ตรวจคำตอบข้อ 1>====================================================#
+#code here
+def proofOne(q:list[float],robot:rtb.DHRobot)->bool:
+    J_e = hand.endEffectorJacobianHW3(q)
+    J_ertb = robot.jacobe(q)
+    allow_error = 0.0001
+    print("-----------Hand Jacobian-----------")
+    print(J_e)
+    print("-----------RTB Jacobian------------")
+    print(J_ertb)
+    print("------------Jacobian True or False------------")
+    return np.allclose(J_e, J_ertb, atol=allow_error)
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 2>====================================================#
 #code here
-
+def proofTwo(q:list[float],robot:rtb.DHRobot)->bool:
+    J = robot.jacobe(q)
+    singularity = hand.checkSingularityHW3(q)
+    if singularity == 1:
+        issingula = True
+    else:
+        issingula = False  
+    singularity_rtb = robot.manipulability(J=J)
+    if singularity_rtb == 0:
+        issingula_rtb = True
+    else:
+        issingula_rtb = False
+    print("-----------Hand Singularity-----------")
+    print(issingula)
+    print("-----------RTB Singularity------------")
+    print(issingula_rtb)
+    print("------------Singularity True or False------------")
+    return issingula == issingula_rtb
+    
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 3>====================================================#
 #code here
-def proofThree(q:list[float], w:list[float])->list[float]:
-    robot = rtb.DHRobot(
-    [
-        rtb.RevoluteMDH(d=0.0892 , offset = pi),
-        rtb.RevoluteMDH(alpha = pi/2 ),
-        rtb.RevoluteMDH(a=-0.425),
-    ],
-    name = "RRR_Robot"
-    )
-    tool_frame = SE3(-0.47443, -0.093,0.109) @ SE3.RPY(0.0,-pi/2,0.0)  # Adjust the position and orientation as needed
-    robot.tool = tool_frame
+def proofThree(q:list[float], w:list[float],robot:rtb.DHRobot)->bool:
+    tau = hand.computeEffortHW3(q,w)
     w = np.array(w)
-    tau = robot.pay(w,q,robot.jacobe(q),1)
-    return -tau
+    print(w)
+    tau_rtb = robot.pay(w,J=robot.jacobe(q),frame = 0)
+    allow_error = 0.0001
+    print("-----------Hand Effort-----------")
+    print(tau)
+    print("-----------RTB Effort------------")
+    print(-tau_rtb)
+    print("------------Effort True or False------------")
+    return np.allclose(tau, -tau_rtb, atol=allow_error)
 #==============================================================================================================#
-# print(proofOne([0.0,0.0,0.0]))
-print(proofThree([0.0,0.0,0.0],[1.0,0.0,0.0,1.0,0.0,0.0]))
+q = hand.q
+w = hand.w
+print(proofOne(q,robot))
+print(proofTwo(q,robot))
+print(proofThree(q,w,robot))
